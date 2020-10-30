7e67f40829c6856f8dc811d707f7ca51b03d4017
Kyle <kyleedwardgjava@gmail.com>
2020-10-30 10:25:17 -0700

    Added Gridview Builder

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/genrelist',
       routes: {
         '/home': (context) => HomeScreen(),
         '/genrelist': (context) => GenreList(),
--- lib/pages/genrelist.dart
+++ lib/pages/genrelist.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:horrify/services/ListOfGenres.dart';
 class GenreList extends StatefulWidget {
   @override
   _GenreListState createState() => _GenreListState();
@@ -10,7 +11,22 @@ class _GenreListState extends State<GenreList> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
+        appBar: AppBar(
+          title: Text('Horrify'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: GridView.builder(
+          itemCount: genres.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () => {print(genres[index]['netflixid'])},
+              child: Card(
+                child: Text(genres[index]['genre']),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
