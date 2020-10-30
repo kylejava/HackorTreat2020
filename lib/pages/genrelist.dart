31c6b8ed61dc9c7aa4709e05a7eac18b7135bcbc
Kyle <kyleedwardgjava@gmail.com>
2020-10-30 09:42:59 -0700

    Added two new screens

---
+++ lib/pages/genrelist.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GenreList extends StatefulWidget {
+  @override
+  _GenreListState createState() => _GenreListState();
+}
+
+class _GenreListState extends State<GenreList> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Horrify'),
+        ),
+      ),
+    );
+  }
+}
